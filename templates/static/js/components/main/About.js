6f1c4a219172160b79d64a9a59306f998e412277
Matthew <msimanonis@gmail.com>
2020-11-16 22:43:04 -0600

    free portrait

--- templates/static/js/components/main/About.js
+++ templates/static/js/components/main/About.js
@@ -8,8 +8,17 @@ const About = () => {
                 <div className='about'>
                     <h1>About Me</h1>
                     <img id='uofi-logo' src='/public/img/uofi.png'></img>
-                    <p>I am a self taught developer, with a B.S. in Molecular and Cellular 
-                        Biology, a Minor in Chemistry, and a certificate of Neuroscience from the University of Illinois in Champaign-Urbana.</p>
+                    <p>I am Matthew Simanonis. I'm a self taught developer, who graduated with a B.S. in Molecular and Cellular 
+                        Biology, a Minor in Chemistry, and a certificate of Neuroscience from the University of Illinois in Champaign-Urbana in 2017.</p>
+                    <br></br>
+                    <p>About 2 years after graduation, I felt stuck at my job, lacking the skills to make a difference, so I took it upon myself to learn
+                        to program. I had some experience in college as an undergraduate research assistant with Python and MATLAB and it was something I 
+                        picked up quickly. Upon starting my first coding class I was hooked. I loved the idea of creating something from scratch and the reward of solving these tough
+                        problems.</p>
+                    <br></br>
+                    <p>My experience is mostly with Python and Javascript, as I enjoy their flexability and robust libraries. This allows me to organize data, display that data
+                        and design helpful web pages. I enjoy using React.js and Flask to host my web apps, as they are simple and allow for a lot of freedom.
+                    </p>
                     <br></br>
                     <p>Though I currently aim to be a Profesional Developer, studying Biology is something I cherish. It taught me strong critical thinking skills, and showed me how cells often think outside of the box, finding creative
                         solutions for tough problems. I've always had an appreciation for the ways the scientific community learned new things, and 
--- templates/static/js/components/main/Info.js
+++ templates/static/js/components/main/Info.js
@@ -10,6 +10,12 @@ const Info = () => {
                 <form action='/public/img/matthew_simanonis_resume.pdf' target="_blank" className="resume">
                     <button className='resume-btn'>Resume </button>
                 </form>
+                <img id='portrait' className='portrait' src={`/public/img/portrait.jpg`}></img>
+                <h4>Hello,</h4>
+                <p>
+                I'm Matt, an aspiring software developer with a background in molecular biology. I believe science and technology all go hand-in-hand; with
+                their focus on logic, data and analytics. With my knowledge of both, I look to make an impact on the world, and help those around me. 
+                </p>
             </div>
         </section>
     )
